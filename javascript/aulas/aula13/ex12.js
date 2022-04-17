2bea969ead72d60499cc099bee3ef04ecae29899
Vando Diniz <vandocarlos2001@outlook.com>
2022-04-17 20:02:31 -0300

    .

---
+++ javascript/aulas/aula13/ex12.js
@@ -0,0 +1,15 @@
+i = 0
+while(i<10){
+    console.log(`While Iteração ${i}`)
+    i++
+}
+
+i = 0
+do {
+    console.log(`Do-While Iteração ${i}`)
+    i++
+} while(i<10)
+
+for(i=0; i<10; i++){
+    console.log(`For Iteração ${i}`)
+}
