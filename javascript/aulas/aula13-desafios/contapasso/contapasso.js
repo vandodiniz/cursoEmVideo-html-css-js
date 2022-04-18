5ebd813f8ed88bef516a716db1997bca399377bb
Vando Diniz <vandocarlos2001@outlook.com>
2022-04-18 13:41:11 -0300

    .

---
+++ javascript/aulas/aula13-desafios/contapasso/contapasso.js
@@ -0,0 +1,11 @@
+var botao = document.getElementById('botao')
+botao.addEventListener('click', contapasso)
+
+function contapasso(){
+    var i = document.getElementById('inicio').value
+    var f = document.getElementById('fim').value
+    var p = document.getElementById('passo').value
+    
+    if (i == null)
+        console.log('null')
+}
