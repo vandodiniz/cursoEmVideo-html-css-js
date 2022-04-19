e735dd021e3691d54789b0f41a89bb40251c2807
Vando Diniz <vandocarlos2001@outlook.com>
2022-04-19 11:02:35 -0300

    Update contapasso.js

--- javascript/aulas/aula13-desafios/contapasso/contapasso.js
+++ javascript/aulas/aula13-desafios/contapasso/contapasso.js
@@ -5,7 +5,14 @@ function contapasso(){
     var i = document.getElementById('inicio').value
     var f = document.getElementById('fim').value
     var p = document.getElementById('passo').value
+
+    if (i == '' || f == '' || p == '') alert('Por favor preencha todos os campos!')
+    else {
+
+        var res = document.getElementById('div2')
+        res.innerHTML = '<hr>Resultado: '
+        for(i = Number(i); i <= Number(f); i+=Number(p)) res.innerHTML += `➜ ${i} `
+        res.innerHTML += '🏁'
+    }
     
-    if (i == null)
-        console.log('null')
 }
