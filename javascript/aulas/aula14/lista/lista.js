0767049751c25e7a0027f1d38eb0cc4b89956db5
Vando Diniz <vandocarlos2001@outlook.com>
2022-04-24 17:00:35 -0300

    exercico

---
+++ javascript/aulas/aula14/lista/lista.js
@@ -0,0 +1,46 @@
+var vetor  = []
+var lista = document.getElementById('lista')
+var res = document.getElementById('div2')
+
+function adicionar(){
+    let number = document.getElementById('num').value
+    let n = Number(number)
+
+    if (number.length == 0 || n <= 0 || n > 100)
+        alert('Insira um número válido!')
+
+    else if (vetor.indexOf(n) != -1)
+        alert('Número já está na lista!')
+
+    else{
+        vetor.push(n)
+        let linha = document.createElement('option')
+        linha.text = `Número ${n} selecionado`
+        lista.append(linha)
+    }
+    res.innerHTML = ''
+}
+
+function calcular(){
+    console.log('entrou')
+    var tam = vetor.length
+
+    if(tam >= 2){
+        var soma = 0
+        for(var pos = 0; pos < tam; pos++)
+            soma += vetor[pos]
+
+        var maior = Math.max.apply(null, vetor)
+        var menor = Math.min.apply(null, vetor)
+
+        res.innerHTML = `<p>Ao todo temos ${vetor.length} números cadastrados. O maior valor é ${maior}  e o menor é ${menor}. A soma de todos os elementos é ${soma} e a média é ${soma/tam}.</p>`
+    }
+
+    else
+        alert('Insira pelo menos 2 elementos na lista')
+}
+
+function limpar(){
+    vetor.length = 0
+    lista.innerHTML = ''
+}
